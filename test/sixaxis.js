4124df015a17d91f3cf3b023a5c8bd9abb17c1bd
Alberto Miranda <codealchemist@gmail.com>
2017-11-01 22:01:26 -0300

    Added sixaxis test.

---
+++ test/sixaxis.js
@@ -0,0 +1,25 @@
+const Sixaxis = require('./src/controllers/sixaxis')
+
+const sixaxis = new Sixaxis()
+sixaxis
+  .on('forward', () => {
+    console.log('WALK FORWARD')
+  })
+  .on('backwards', () => {
+    console.log('WALK BACKWARDS')
+  })
+  .on('left', () => {
+    console.log('TURN LEFT')
+  })
+  .on('right', () => {
+    console.log('TURN RIGHT')
+  })
+  .on('sleep', () => {
+    console.log('BYE!')
+  })
+  .on('stop', () => {
+    console.log('STOP')
+  })
+  .on('attack', () => {
+    console.log('TATAKAU!')
+  })
